4c5b63d0bcbafe27ea754289769e91b870f7592a
Alexander Meyer-Gohde <meyer-gohde@econ.uni-frankfurt.de>
2024-06-12 10:53:37 +0200

    Update mmb_loop_Max.m
    used new version of forward errors, this allows for a minimal version for very large models

--- Main_files/Research_routine/mmb_loop_Max.m
+++ Main_files/Research_routine/mmb_loop_Max.m
@@ -5,7 +5,7 @@
 
  
 
-run_time_reps=2;
+run_time_reps=1;
 newton_options.dynare_reduced_sylvester=1;
 newton_options.maximum_iterations=1000;
 addpath('C:\dynare\5.1\matlab')
@@ -37,7 +37,7 @@ mmb_vec= ot.model_name(logical(ot.model_folder_exists));
 
 loop_n=size(mmb_vec,1);
 % MTCHANGE: preallocate with nan instead of zeros
-AMG_JS_Results=nan(8,7,loop_n);
+AMG_Results=nan(17,7,loop_n);
 
 % MTCHANGE: add variable start and end to looping (also change in for loop
 % start)
@@ -68,13 +68,13 @@ try
     eval(['dynare ', mmb_vec{loop_k}, '_rep noclearall nograph nostrict  nolog'])
     %%%%% current problem: dynare_to_matrix_quadratic needs to be located in
     %%%%% mmb-rep-folders (e.g. BRA_SAMBA08_rep)
-    AMG_JS_Results(1,:,loop_k)=[M_.nstatic, M_.nfwrd, M_.npred, M_.nboth, M_.nsfwrd, M_.nspred, M_.ndynamic];
+    AMG_Results(1,:,loop_k)=[M_.nstatic, M_.nfwrd, M_.npred, M_.nboth, M_.nsfwrd, M_.nspred, M_.ndynamic];
     
     
     [matrix_quadratic, jacobia_]=create_reduced_matrix_quadratic_from_dynare(M_,oo_);
     
     %tic; [info, oo_, options_]  = stoch_simul(M_, options_, oo_, var_list_); toc    
-     tic; for jj=1:run_time_reps; [dr,info] = dyn_first_order_solver(jacobia_,M_,oo_.dr,options_,0); end;   AMG_JS_Results(2,1,loop_k) = toc/run_time_reps;   
+     tic; for jj=1:run_time_reps; [dr,info] = dyn_first_order_solver(jacobia_,M_,oo_.dr,options_,0); end;   AMG_Results(2,1,loop_k) = toc/run_time_reps;   
     
      
      
@@ -82,12 +82,12 @@ try
     X_dynare=ALPHA_ZS_dynare;
     matrix_quadratic.X=ALPHA_ZS_dynare;
     try
-    [errors] = dsge_practical_forward_errors_matrix_quadratic(matrix_quadratic);
-    AMG_JS_Results(2,3:5,loop_k)=errors([4,7,8],1)';
+    if M_.endo_nbr>100; [errors]=dsge_backward_errors_condition_sparse_minimal(matrix_quadratic); else [errors]=dsge_backward_errors_condition_full(matrix_quadratic);end
+    AMG_Results(3:end,1:5,loop_k)=errors;
     ot.error_flag(strcmp(ot.model_name,mmb_vec{loop_k})) = 0;
     ot.error(strcmp(ot.model_name,mmb_vec{loop_k})) = string("N/A");
     catch
-    AMG_JS_Results(2,3:5,loop_k)=NaN(1,3);
+    AMG_Results(3:end,1:5,loop_k)=NaN(15,5);
     end
     
 catch ME
